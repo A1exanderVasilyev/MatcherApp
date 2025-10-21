23c2a9fc9cd6f1c8475cf965f8a09de25c2bcd7c
catinasw <you@example.com>
2025-10-21 12:05:22 +0300

    made user repo

---
+++ src/main/java/ru/vasilyev/MatcherApp/repositoies/UserRepository.java
@@ -0,0 +1,13 @@
+package ru.vasilyev.MatcherApp.repositoies;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.vasilyev.MatcherApp.models.User;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findUserByUsername(String username);
+}
