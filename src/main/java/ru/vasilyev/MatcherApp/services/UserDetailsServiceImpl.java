676543058855f959f071b51d9576651e98a6ddf3
catinasw <alexandrvasilev12rus@gmail.com>
2025-10-22 15:31:03 +0300

    implemented user details service

--- src/main/java/ru/vasilyev/MatcherApp/services/UserDetailsServiceImpl.java
+++ src/main/java/ru/vasilyev/MatcherApp/services/UserDetailsServiceImpl.java
@@ -5,7 +5,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import ru.vasilyev.MatcherApp.models.User;
 import ru.vasilyev.MatcherApp.repositoies.UserRepository;
+import ru.vasilyev.MatcherApp.security.UserDetailsImpl;
+
+import java.util.Optional;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -17,7 +21,11 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     }
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findUserByEmail(email);
+        if (!user.isPresent()) {
+            throw new UsernameNotFoundException("Bad credentials");
+        }
+        return new UserDetailsImpl(user.get());
     }
 }
