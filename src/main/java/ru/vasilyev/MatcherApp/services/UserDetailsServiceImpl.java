eadec5e9ab07dd43e4f7fe9efd06655b86384210
catinasw <alexandrvasilev12rus@gmail.com>
2025-10-22 15:26:13 +0300

    implemented user details

---
+++ src/main/java/ru/vasilyev/MatcherApp/services/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package ru.vasilyev.MatcherApp.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.vasilyev.MatcherApp.repositoies.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
