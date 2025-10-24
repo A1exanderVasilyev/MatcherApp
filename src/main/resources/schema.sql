2caaf4b4554c039e951c0ab26b78e1b36bf7822f
catinasw <alexandrvasilev12rus@gmail.com>
2025-10-24 12:56:16 +0300

    added current tables state

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,20 +1,39 @@
 drop table if exists users cascade;
 
 CREATE TABLE users (
-                       id BIGSERIAL PRIMARY KEY,
-                       username VARCHAR(100) NOT NULL,
-                       password VARCHAR(255) NOT NULL,
-                       date_of_birth DATE NOT NULL,
-                       gender VARCHAR(20) NOT NULL CHECK (gender IN ('MALE', 'FEMALE'),
-                       email VARCHAR(255) NOT NULL UNIQUE,
-                       country VARCHAR(100) NOT NULL,
-                       city VARCHAR(100) NOT NULL,
-                       latitude DECIMAL(9,6) ,
-                       longitude DECIMAL(10,6) ,
-                       role VARCHAR(20),
-                       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                       updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    id BIGSERIAL PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    gender VARCHAR(20) NOT NULL CHECK (gender IN ('MALE', 'FEMALE'),
+    email VARCHAR(255) NOT NULL UNIQUE,
+    country VARCHAR(100) NOT NULL,
+    city VARCHAR(100) NOT NULL,
+    latitude DECIMAL(9,6) ,
+    longitude DECIMAL(10,6) ,
+    role VARCHAR(20),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
-                       CONSTRAINT chk_latitude_range CHECK (latitude >= -90.0 AND latitude <= 90.0),
-                       CONSTRAINT chk_longitude_range CHECK (longitude >= -180.0 AND longitude <= 180.0)
+    CONSTRAINT chk_latitude_range CHECK (latitude >= -90.0 AND latitude <= 90.0),
+    CONSTRAINT chk_longitude_range CHECK (longitude >= -180.0 AND longitude <= 180.0)
+);
+
+DROP TABLE IF EXISTS users_photo CASCADE;
+
+CREATE TABLE users_photo (
+    id BIGSERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    photo_url VARCHAR(500) NOT NULL,
+    file_name VARCHAR(255),
+    file_size BIGINT,
+    uploaded_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    is_primary BOOLEAN DEFAULT FALSE,
+
+    CONSTRAINT fk_user_photo_user FOREIGN KEY (user_id) REFERENCES users(id)
+        ON DELETE CASCADE,
+
+    UNIQUE(user_id, is_primary),
+
+    CONSTRAINT chk_file_size CHECK (file_size <= 10485760)
 );
