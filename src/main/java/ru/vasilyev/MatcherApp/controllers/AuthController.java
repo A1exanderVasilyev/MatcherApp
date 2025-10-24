be889b88a3e106f19087c14b094f56c54d19b272
catinasw <alexandrvasilev12rus@gmail.com>
2025-10-24 12:35:12 +0300

    rewrite registration process with dto and handle photo uploading

--- src/main/java/ru/vasilyev/MatcherApp/controllers/AuthController.java
+++ src/main/java/ru/vasilyev/MatcherApp/controllers/AuthController.java
@@ -2,15 +2,16 @@ package ru.vasilyev.MatcherApp.controllers;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import ru.vasilyev.MatcherApp.dto.UserRegistrationDTO;
 import ru.vasilyev.MatcherApp.enums.Gender;
-import ru.vasilyev.MatcherApp.models.User;
 import ru.vasilyev.MatcherApp.services.RegistrationService;
-
-import java.time.LocalDateTime;
+import ru.vasilyev.MatcherApp.util.exceptions.UserExistsException;
 
 @Controller
 @RequestMapping("/auth")
@@ -23,26 +24,38 @@ public class AuthController {
     }
 
     @GetMapping("/registration")
-    public String registrationPage(@ModelAttribute("user") User user,
+    public String registrationPage(@ModelAttribute("user") UserRegistrationDTO userRegistrationDTO,
                                    Model model) {
         model.addAttribute("genders", Gender.values());
         return "auth/registration";
     }
 
-    @PostMapping("/registration")
-    public String processRegistration(@ModelAttribute("user") @Valid User user,
+    @PostMapping(value = "/registration", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String processRegistration(@ModelAttribute("user") @Valid UserRegistrationDTO userRegistrationDTO,
                                       BindingResult bindingResult,
                                       Model model) {
+        if (!userRegistrationDTO.hasProfilePhoto()) {
+            bindingResult.rejectValue("profilePhoto", "error.user", "Выберите фото профиля");
+        } else {
+            photoValidation(userRegistrationDTO.getProfilePhoto(), bindingResult);
+        }
+
         if (bindingResult.hasErrors()) {
             model.addAttribute("genders", Gender.values());
             return "auth/registration";
         }
-        user.setRole("ROLE_USER");
-        LocalDateTime currentDate = LocalDateTime.now();
-        user.setCreatedAt(currentDate);
-        user.setUpdatedAt(currentDate);
-        registrationService.register(user);
-        return "redirect:/auth/login";
+
+        try {
+            registrationService.register(userRegistrationDTO);
+            model.addAttribute("success", "Регистрация прошла успешно! Теперь вы можете войти.");
+            return "redirect:/auth/login";
+        } catch (UserExistsException e) {
+            bindingResult.rejectValue("email", "error.user", "Email уже занят");
+        } catch (Exception e) {
+            model.addAttribute("error", "Ошибка при регистрации");
+        }
+        model.addAttribute("genders", Gender.values());
+        return "/auth/registration";
     }
 
     @GetMapping("/login")
@@ -53,5 +66,16 @@ public class AuthController {
         }
 
         return "auth/login";
+    }
+
+    private void photoValidation(MultipartFile photo, BindingResult result) {
+        if (photo.getSize() > 5 * 1024 * 1024) {
+            result.rejectValue("profilePhoto", "error.user", "Размер файла не должен превышать 5MB");
+        }
+
+        String contentType = photo.getContentType();
+        if (contentType != null && !contentType.startsWith("image/")) {
+            result.rejectValue("profilePhoto", "error.user", "Файл должен быть изображением");
+        }
     }
 }
